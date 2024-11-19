cc1674ff1086997ff825370cb80e98c0b100819a
JosephRidge <josephridge18@gmail.com>
2024-11-19 12:02:51 +0300

    introduction

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { createMemoryHistory, createRouter } from "vue-router";
 
-createApp(App).mount("#app");
+import Introduction from "./components/pages/Introduction.vue";
+
+const routes = [
+  { path: "/", component: Introduction },
+  { path: "/about", component: Introduction },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
