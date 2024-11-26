85e02ec873407c50d2b3ddbec046803bb8614b3a
JosephRidge <josephridge18@gmail.com>
2024-11-26 21:08:40 +0300

    KTAS KPIs

--- src/components/pages/Expert.vue
+++ src/components/pages/Expert.vue
@@ -1,52 +1,119 @@
 <template>
-  <div class=" px-24 text-left w-screen h-screen tracking-widest leading-loose">
-    <div class="font-bold text-4xl py-2">
-      Focusing on Triage Diagnosis: Nurse-Expert Accuracy
-    </div>
- 
-
-    <div class=" py-10 w-screen h-fit ">
-      <table class=" ">
-        <thead>
-          <tr class="bg-gray-200 w-screen">
-            <th></th>
-            <!-- Empty cell for top-left corner -->
-            <th class="bg-black text-white px-4">Nurse Remarks</th>
-            <th></th>
-          </tr>
-          <tr>
-            <th class="bg-black text-white px-4">Expert Remarks</th>
-            <th class="font-normal bg-gray-200 px-3">Emergency</th>
-            <th class="font-normal px-3 bg-gray-200">Non-Emergency</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="row-header px-3 bg-gray-200">Emergency</td>
-            <td class="px-3">{{ expertNurseEmergencyAlignment }}</td>
-            <td class="px-3 bg-red-500 font-bold">
-              {{ expertNurseEmergencyNonEmergencyAlignment }}
-            </td>
-          </tr>
-          <tr>
-            <td class="row-header px-3 bg-gray-200">Non-Emergency</td>
-            <td class="px-3 bg-red-500 font-bold">
-              {{ expertNurseNonEmergencyEmergencyAlignment }}
-            </td>
-            <td class="px-3">
-              {{ expertNurseNonEmergencyNoEmergencyAlignment }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div
+    class="text-left w-screen h-screen tracking-widest bg-gray-100 leading-loose"
+  >
+    <div class="font-bold text-4xl py-4 px-24">
+      Korean Triage and Acuity Scale KPIs
     </div>
 
-    <RouterLink
-      to="/causesOfmistriage"
-      class="rounded-full bg-gray-950 hover:bg-black w-fit px-4 py-2 my-6 scale-90 hover:cursor-pointer text-white"
-    >
-      Causes of Mistriage
-    </RouterLink>
+    <div class="px-24 py-5 w-screen h-fit flex justify-evenly">
+      <!-- confusion matrix -->
+      <div class="bg-white p-auto shadow-lg rounded-md">
+        <table class="rounded-md m-4">
+          <thead>
+            <tr class="bg-white w-screen">
+              <th></th>
+              <!-- Empty cell for top-left corner -->
+              <th class="bg-black text-white px-4">Nurse Remarks</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th class="bg-black text-white px-4">Expert Remarks</th>
+              <th class="font-normal bg-white px-3">Emergency</th>
+              <th class="font-normal px-3 bg-white">Non-Emergency</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="row-header px-3 bg-white">Emergency</td>
+              <td class="px-3">{{ expertNurseEmergencyAlignment }}</td>
+              <td class="px-3 bg-red-500 font-bold">
+                {{ expertNurseEmergencyNonEmergencyAlignment }}
+              </td>
+            </tr>
+            <tr>
+              <td class="row-header px-3 bg-white">Non-Emergency</td>
+              <td class="px-3 bg-red-500 font-bold">
+                {{ expertNurseNonEmergencyEmergencyAlignment }}
+              </td>
+              <td class="px-3">
+                {{ expertNurseNonEmergencyNoEmergencyAlignment }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- arrow -->
+      <!-- <div class=" flex justify-end rotate-180 "> <img src="../../assets/illustrations/arrow.svg" class="w-24" alt="" srcset=""></div> -->
+    </div>
+
+    <div class="flex gap-1 scale-95">
+      <!-- accuracy -->
+      <div
+        class="grid bg-white border-2 border-blue-200 hover:border-blue-400 shadow-lg p-8 rounded-xl w-3/12"
+      >
+        <div class="font-bold text-3xl text-center">Accuracy</div>
+        <div class="text-5xl m-3 text-center text-gray-500 font-light">
+          91.9 <span class="text-5xl text-gray-200">%</span>
+        </div>
+        <hr />
+        <div class="text-center">
+          Nurses correctly classified 91.9% of the cases(high level of agreement
+          with expert)
+        </div>
+      </div>
+      <!-- Precision -->
+      <div
+        class="grid bg-white shadow-lg border-2 border-blue-200 hover:border-blue-400 p-8 rounded-xl w-3/12"
+      >
+        <div class="font-bold text-3xl text-center">Precision</div>
+        <div class="text-5xl m-3 text-center text-gray-500 font-light">
+          96.5 <span class="text-5xl text-gray-200">%</span>
+        </div>
+        <hr />
+        <div class="text-center">
+          When nurses say a case is an emergency, they are correct 96.5% of the
+          time
+        </div>
+      </div>
+      <!-- Sensitivity -->
+      <div
+        class="grid bg-white shadow-lg border-2 border-blue-200 hover:border-blue-400 p-8 rounded-xl w-3/12"
+      >
+        <div class="font-bold text-3xl text-center">Sensitivity</div>
+        <div class="text-5xl m-3 text-center text-gray-500 font-light">
+          89.4 <span class="text-5xl text-gray-200">%</span>
+        </div>
+        <hr />
+        <div class="text-center">
+          Nurses correctly identify 89.4% of all emergencies, but 10.6% of
+          emergencies are missed
+        </div>
+      </div>
+      <!-- specificity -->
+      <div
+        class="grid bg-white shadow-lg border-2 border-blue-200 hover:border-blue-400 p-8 rounded-xl w-3/12"
+      >
+        <div class="font-bold text-3xl text-center">Specificity</div>
+        <div class="text-5xl m-3 text-center text-gray-500 font-light">
+          95.5 <span class="text-5xl text-gray-200">%</span>
+        </div>
+        <hr />
+        <div class="text-center">
+          Nurses correctly identify 95.5% of all non-emergencies, with only a
+          small error rate
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center">
+      <RouterLink
+        to="/causesOfmistriage"
+        class="rounded-full bg-gray-950 hover:bg-black w-fit px-4 py-2 my-6 hover:cursor-pointer text-white"
+      >
+        But what are the Causes of Mistriage ?
+      </RouterLink>
+    </div>
   </div>
 </template>
 
@@ -117,7 +184,7 @@ export default {
       return Object.keys(counts).map((type) => ({
         label: type,
         value: counts[type],
-        color: type.includes("Emergency") ? "red" : "black",
+        color: type.includes("Emergency") ? "blue" : "black",
       }));
     },
     async matrix() {
@@ -135,7 +202,7 @@ export default {
               groups: ["label"],
               backgroundColor: (ctx) => {
                 const type = ctx.raw ? ctx.raw.label : "";
-                return type.includes("Emergency") ? "red" : "black";
+                return type.includes("Emergency") ? "blue" : "black";
               },
               borderWidth: 1,
               borderColor: "white",
